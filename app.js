4535bfecf4e7f54d33305f91159570982a3bef0f
ReyesRodolfo <152461419+ReyesRodolfo@users.noreply.github.com>
2025-12-17 08:38:16 -0600

    Implement simulated login and logout functionality

--- app.js
+++ app.js
@@ -10,10 +10,37 @@ function login() {
 
   // LOGIN SIMULADO
   if (email === "admin@uni.edu" && password === "1234") {
+
+    // Guardamos sesión
+    localStorage.setItem("usuario", email);
+
     mensaje.textContent = "Login correcto ✅";
     mensaje.style.color = "green";
+
+    setTimeout(() => {
+      window.location.reload();
+    }, 1000);
+
   } else {
     mensaje.textContent = "Credenciales incorrectas ❌";
     mensaje.style.color = "red";
   }
 }
+
+window.onload = () => {
+  const usuario = localStorage.getItem("usuario");
+
+  if (usuario) {
+    document.body.innerHTML = `
+      <h2>Bienvenido ${usuario}</h2>
+      <p>Sesión activa</p>
+      <button onclick="logout()">Cerrar sesión</button>
+    `;
+  }
+};
+
+function logout() {
+  localStorage.removeItem("usuario");
+  window.location.reload();
+}
+
